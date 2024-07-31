3ae70f95a742a81806d7816ef43e9e235e2ba8e7
AlkaSinha5 <sinhaalka802211@gmail.com>
2024-07-31 16:01:55 +0530

    send mail  done

---
+++ src/helper/sendMail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (email, subject,text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: "contacthospitaldoctor@gmail.com",
+            service: "gmail",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "contacthospitaldoctor@gmail.com",
+                pass: "vdxu hvgq ucnx xkzc",
+            },
+        });
+    
+        
+        await transporter.sendMail({
+            from: "contacthospitaldoctor@gmail.com",
+            to: email,
+            subject: subject, 
+            text:text,
+            
+        });
+
+        console.log("email sent sucessfully");
+    } catch (error) {
+        console.log(error, "email not sent");
+    }
+};
+
+module.exports = sendEmail;
